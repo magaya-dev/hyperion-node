37957648f29450a3a4c102aab22cbc36cfff8eb6
Jorge Cabot <jcabot@magaya.com>
2018-02-27 19:07:26 -0500

    Added proper debugging.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const os = require('os');
-const addon = require(`./bin/${os.arch()}/hyperion`);
-const Algorithms = require('@magaya/hyperion-algorithms');
+const debug = require('debug')('hyperion-addon'); debug('Loaded debug...');
+const os = require('os'); debug('Loaded os...');
+const addon = require(`./bin/${os.arch()}/hyperion`); debug('Loaded hyperion addon...');
+const Algorithms = require('@magaya/hyperion-algorithms'); debug('Loaded Algorithms...');
 
 module.exports = function({ name, argv }) {
+    debug(`Attempting to connect as ${name} with: ${argv}`);
+
     const connection = addon.connect(name, argv);
+
+    debug('We connected...');
+    debug(`Hyperion status: ${connection.hyperion}`);
 
     return {
         algorithm: new Algorithms(connection.async),
