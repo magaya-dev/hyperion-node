c4c1bcd441df8abadc927f8fce8ca1eecfa5a563
Jorge Cabot <jcabot@jorgecabot.com>
2018-09-07 13:07:36 -0400

    Added new integration test to ensure saving to database works as expected.

--- spec/integration/connectionIntegrationTest.js
+++ spec/integration/connectionIntegrationTest.js
@@ -1,3 +1,7 @@
+/**
+ * Ensure we can connect with and without an api request.
+ */
+
 const assert = require('assert');
 const hyperion = require('../../index');
 
@@ -6,9 +10,13 @@ const connectionWithoutApi = hyperion(["--connection-string=test.magaya.com:6110
 
 assert.ok(connectionWithoutApi, 'no connection');
 assert.ok(connectionWithoutApi.dbx, 'no hyperion');
+assert.ok(connectionWithoutApi.dbw, 'no write access');
+assert.ok(connectionWithoutApi.algorithm, 'no algorithm');
 
 // Ensure we can connect by specifying an API
 const connectionWithApi = hyperion(["--connection-string=test.magaya.com:6110"], 'livetrack');
 
 assert.ok(connectionWithApi, 'no connection');
 assert.ok(connectionWithApi.dbx, 'no hyperion');
+assert.ok(connectionWithApi.dbw, 'no write access');
+assert.ok(connectionWithApi.algorithm, 'no algorithm');
