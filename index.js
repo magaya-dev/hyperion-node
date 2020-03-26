c4960b4515a6e48f1e3142d9b187a801caf4a78f
Jorge Cabot <jcabot21@gmail.com>
2020-03-26 15:45:29 -0400

    added temp BETA url. added some jsdoc. minor code update.

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const Algorithms = require('@magaya/hyperion-algorithms'); debug('Loaded Algorit
 const WriteAccess = require('@magaya/hyperion-write-access'); debug('Loaded WriteAccess...');
 
 /**
+ * @typedef ApiConfig
+ * @type {object}
+ * @property {string} clientId - Unique name for the extension.
+ * @property {string} apiKey - API key for the extension.
+ * @property {string} scope - Used in case no apiKey is provided, will dictate extension permissions.
+ * @property {string} apiKeyPath - Used in case no apiKey is provided, will dictate where keys are stored.
+ */
+
+/**
  * @typedef HyperionConnectionHandle
  * @type {object}
  * @property {object} algorithm Namespace for algorithm functions.
@@ -19,14 +28,15 @@ const WriteAccess = require('@magaya/hyperion-write-access'); debug('Loaded Writ
  * in the list of provided arguments.
  * 
  * @param {string[]} argv List of command line arguments for program.
- * @param {string} [api] Optional name for requested api
+ * @param {string | ApiConfig } [api] Optional name for requested api
  * 
  * @returns {HyperionConnectionHandle} Handle for hyperion connection.
  */
 module.exports = (argv, api) => {
     debug(`Attempting to connect with: ${argv}`);
 
-    const token_url = "http://localhost:5000/connect/token"; // !TODO(jose): handle this
+    // TODO: Temporary URL for BETA
+    const token_url = "https://magaya-identity-web.azurewebsites.net/connect/token"; 
     const client_id = api && api.clientId ? api.clientId : api;
 
     if (api) {
@@ -39,18 +49,22 @@ module.exports = (argv, api) => {
 
     const connection = addon.connect(argv, api);
 
-    debug('We connected...');
+    debug('Database connected...');
     debug(`Hyperion defined: ${connection.hyperion !== undefined && connection.hyperion !== null}`);
 
     if (api) {
-        // !NOTE(jose): promisify call to get access token
+        // Promisify call to get access token
         const api_obj = connection[client_id];
-        if (api_obj && api_obj.getAccessToken) {
-            const orginal = api_obj.getAccessToken;
-            api_obj.getAccessToken = () => new Promise((resolve, reject) => orginal(token_url, client_id, (err, token) => {
-                if (err) reject(err);
-                else resolve(token);
-            }));
+
+        if (api_obj && api_obj.getAccessToken) {            
+            const original = api_obj.getAccessToken;
+
+            api_obj.getAccessToken = () => 
+                new Promise((resolve, reject) => 
+                    original(token_url, client_id, (err, token) => 
+                        err ? reject(err) : resolve(token)));
+
+            debug(`${client_id} setup to request access tokens...`);
         }
     }
 
